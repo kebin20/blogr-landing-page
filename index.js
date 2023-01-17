e2afcad6038c984896e9d0900e413a06fd2413f8
kebin20 <ktanzyl@hotmail.com>
2023-01-17 11:23:14 +0900

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import stylesheets
-import './style.css';
+// import './style.css';
 
 const arrowProduct = document.getElementById('arrow-product');
 const arrowCompany = document.getElementById('arrow-company');
